5c7e38b701ea1579333738776bd78c4daf335e87
Areena-Dilawar <areenadilawar@gmail.com>
2024-10-29 21:28:38 +0500

    Update Index.js

--- Index.js
+++ Index.js
@@ -2,25 +2,23 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 let books = [
-  { id: 1, title: '1984', author: 'George Orwell', year: 1949 },
-  { id: 2, title: 'To Kill a Mockingbird', author: 'Harper Lee', year: 1960 }
+    { id: 1, title: '1984', author: 'George Orwell', year: 1949 },
+    { id: 2, title: 'To Kill a Mockingbird', author: 'Harper Lee', year: 1960 }
 ];
 
-app.post('/add', (req, res)=>{
-const newbook = {
-    id: books.length+1,
-    title: req.body.title,
-    author: req.body.author,
-    year: req.body.year,
-};
-books.push(newbook)
-req.send('Book added successfully')
+app.post('/add', (req, res) => {
+    res.send('Book added ')
+});
+app.get('/list', (req, res) => {
+    res.json(books);
+});
+app.put('/update', (req, res) =>{
+    res.send('Book Updated');
+})
+app.delete('/delete/:id', (req, res) => {
+    res.send('Book deleted ');
 });
 
-app.get('/list', (req, res) => {
-  res.json(books);  
-});
-  
 app.listen(8000, () => {
-  console.log('Server is running on port 8000');
+    console.log('Server is running on port 8000');
 })
