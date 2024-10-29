080ffe285fcede60ce4aa3ca551385fa2b94e815
Areena-Dilawar <areenadilawar@gmail.com>
2024-10-29 20:45:43 +0500

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => {
-    res.send('welcome')
-})
-app.listen(8000, ()=>{
-    console.log('server is running on port 8000')
-})
+
+let books = [
+  { id: 1, title: '1984', author: 'George Orwell', year: 1949 },
+  { id: 2, title: 'To Kill a Mockingbird', author: 'Harper Lee', year: 1960 }
+];
+
+
+app.get('/welcome', (req, res) => {
+  res.send('Welcome');
+});
+
+app.get('/list', (req, res) => {
+  res.json(books);  
+});
+
+app.listen(8000, () => {
+  console.log('Server is running on port 8000');
+});
